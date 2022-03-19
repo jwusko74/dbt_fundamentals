4802fce639656db0d9af99a0e105a637f497435d
Jeff Usko <jwusko@gmail.com>
2022-03-19 11:39:33 +0000

    rev03192022

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,14 +1,13 @@
-with payments as (
-    
+with payment as (
     select
-        id as pyament_id,	
+        id as payment_id,	
         orderid as order_id,	
         paymentmethod as payment_method,	
         status,
         amount/100 as amount,
         created as created_at
-    from raw.stripe.payment
+    from {{source('stripe','payment')}}    
 )
 
-select * from payments
+select * from payment
 
